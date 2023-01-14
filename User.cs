308ad1e7be8e43c0d819edc9219543dafb753292
Frans-Mathapo <87014514+Frans-Mathapo@users.noreply.github.com>
2023-01-14 03:06:01 +0200

    user class

---
+++ User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrapper
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string FindMessage { get; set; } 
+        public string Date { get; set; }
+
+
+}
+}
